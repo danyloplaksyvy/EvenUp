5577ebf9b46ec2602a12d8c3099908d476332875
danyloplaksyvy <danyloplaksyvy@gmail.com>
2026-06-15 14:41:05 +0200

    gitignore full setup with rm cached

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,16 +5,12 @@ plugins {
 
 android {
     namespace = "com.dps.evenup"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.dps.evenup"
         minSdk = 29
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
